90ab83f13449d1e0b4e517b364bf39fa3dac7893
Paramet Prasongngern <pspeedtime@gmail.com>
2024-04-17 18:23:12 +0700

    4/14/2024 home.jsx

--- Maincontent/Home.jsx
+++ Maincontent/Home.jsx
@@ -1,12 +1,14 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { TextInput, Button  } from 'react-native-paper';
+import React, { useState, useEffect } from 'react';
+import { black, white } from 'react-native-paper/lib/typescript/styles/themes/v2/colors';
 
 const Home = () => {
   return (
     <View style={{flex : 1}}>
-      <View style={{alignItems : 'flex-start', justifyContent : 'space-between',flexDirection : 'row'}}>
-        <View>
-          <Image style={{ width : 68 , height : 36 }} source={require('../img/info.png')}></Image>
+      <View style={{alignItems : 'flex-start', justifyContent : 'space-between',flexDirection : 'row' }}>
+        <View style={{flexDirection : 'row' , paddingTop : 20 , paddingLeft : 10 }}>
+          <Image source={require('../img/info.png')}></Image>
         </View>
         <View style={{flexDirection : 'row'}}>
           <View style={{ width : 50 , height : 50 , backgroundColor : 'black'}}></View>
